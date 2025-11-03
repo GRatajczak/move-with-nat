9d1dd13beb78522d56a31d04bb0ae75594a9976e
Grzegorz Ratajczak <g.ratajczak97@gmail.com>
2025-11-03 22:25:22 +0100

    update supabase client

--- src/db/supabase.client.ts
+++ src/db/supabase.client.ts
@@ -1,14 +1,8 @@
-import { createClient, type SupabaseClient } from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
 
 import type { Database } from "../db/database.types.ts";
 
 const supabaseUrl = import.meta.env.SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.SUPABASE_KEY;
 
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error("Missing SUPABASE_URL or SUPABASE_KEY environment variables");
-}
-
-export const supabaseClient: SupabaseClient<Database> = createClient<Database>(supabaseUrl, supabaseAnonKey);
-
-export type { SupabaseClient };
+export const supabaseClient = createClient<Database>(supabaseUrl, supabaseAnonKey);
